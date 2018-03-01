4a8d1c542020985c8b6e6f2acd4de04de966e214
李春燕 <aimeeli@lichunyandeMacBook-Pro.local>
2018-03-01 18:43:06 +0800

    blog

--- src/containers/blogMain/MajorList.js
+++ src/containers/blogMain/MajorList.js
@@ -21,7 +21,7 @@ export default class MajorList extends Component {
             上个月3月3号的时候，同事推荐说买可以0元购买斐讯K2路由器，看办公室人员有几个买了，博主也半信半疑的尝试买了一个，试试看是不是真的能返还钱。截止今天，钱终于退回来了。所以才敢给大家推荐，顺便给大家详细介绍下流程。
           </section>
           <section className="article-label">
-            <span>rect学习系列</span>
+            <span>rect学习系列  </span>
             
             <button className="article-all">阅读全文 >></button>
           </section>
