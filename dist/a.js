45dd625f63f188a4f59f469f4f34f906f2a7f185
李春燕 <aimeeli@lichunyandeMacBook-Pro.local>
2018-09-14 18:12:02 +0800

    huancun

--- dist/a.js
+++ dist/a.js
@@ -1,23 +1,23 @@
 webpackJsonp([1],{
 
-/***/ "./src/a.js":
+/***/ "6LK4":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "aQZI":
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__("./src/c.css");
+__webpack_require__("6LK4");
 
 // 公共文件
 var a = 1; // import './a.css'; // A项目的文件
 
-/***/ }),
-
-/***/ "./src/c.css":
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
 /***/ })
 
-},["./src/a.js"]);
+},["aQZI"]);
--- dist/b.js
+++ dist/b.js
@@ -1,32 +1,32 @@
 webpackJsonp([0],{
 
-/***/ "./src/b.css":
+/***/ "6LK4":
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
 
 /***/ }),
 
-/***/ "./src/b.js":
+/***/ "Fwn7":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "JIOT":
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__("./src/b.css");
+__webpack_require__("Fwn7");
 
-__webpack_require__("./src/c.css");
+__webpack_require__("6LK4");
 
 // 公共文件
 var b = 2; // b项目单独的文件
 
-/***/ }),
-
-/***/ "./src/c.css":
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
 /***/ })
 
-},["./src/b.js"]);
+},["JIOT"]);
